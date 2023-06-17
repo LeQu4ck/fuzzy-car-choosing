707295946a0af68b25e4742bf90b81b0c06ed9c3
Cobeanu Andrei <taviandrey@yahoo.com>
2023-06-17 20:19:26 +0300

    second

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/primevue.min.css";
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+app.use(PrimeVue, {ripple: true});
 
 app.mount('#app')
